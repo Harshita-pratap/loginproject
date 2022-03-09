0bf88963aa73f412f945bfa618aabeccc8282bd3
Harshita <harshita.sai@modmed.com>
2022-03-09 15:28:43 +0530

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component:UserloginComponent},
+  {path:'loginpage', component:LoginpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserloginComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
